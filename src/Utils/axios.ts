ac458ceb83c94a70c385254c49933cb09d233b56
iamtheef <eftihis.riise@gmail.com>
2020-11-25 11:56:31 +0200

    <>

--- src/Utils/axios.ts
+++ src/Utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const client = axios.create({
-  baseURL: "http://0.0.0.0:4000",
+  baseURL: "http://localhost:4000",
   timeout: 30000,
-  headers: { "X-Custom-Header": "foobar" },
+  headers: { "X-Custom-Header": "foobar", "Content-Type": "application/json" },
 });
