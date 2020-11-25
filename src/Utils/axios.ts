aaedf9c365c4d5a2afd0222a18438bd72fb4c80e
iamtheef <eftihis.riise@gmail.com>
2020-11-25 19:57:28 +0200

    testing

--- src/Utils/axios.ts
+++ src/Utils/axios.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
-let isProduction = process.env.REACT_APP_ENV == "production";
-
+let isProduction = process.env.REACT_APP_ENV?.toString() == "production";
+console.log(isProduction);
+console.log(process.env.REACT_APP_api);
+console.log(process.env.REACT_APP_local);
 export const client = axios.create({
   baseURL: isProduction
     ? process.env.REACT_APP_api
