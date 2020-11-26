63dcd27ffd3bd5c3d603251a8582fd6fc5956cd3
iamtheef <eftihis.riise@gmail.com>
2020-11-26 10:57:30 +0200

    changed the port

--- server_.js
+++ server_.js
@@ -5,8 +5,8 @@
 
   app.use(express.static(path.join(__dirname, "build")));
 
-  app.listen(443);
-  console.log("Serving on 443");
+  app.listen(80);
+  console.log("Serving on 80");
   console.log("*".repeat(10));
   console.log("Love is forever...");
 })();
